63ef4b7b9532de4babee7ee9442aa9f8c23cf4c5
unknown <albertespen@gmail.com>
2015-11-07 10:54:36 +0800

    Added A1-3, 300 annotated documents + a fix to the contextExtractor and printing of every comment given to the wrongClassificationPrinter

--- WrongClassificationPrinter.py
+++ WrongClassificationPrinter.py
@@ -4,3 +4,5 @@ def printWrongClassifiedComments(testLabels, predictionLabels, testData):
     for i in range(len(testLabels)):
         if testLabels[i] != predictionLabels[i]:
             print("Misclassified: ", testData[i].encode('ascii', 'ignore'), predictionLabels[i], testLabels[i])
+        else:
+            print("Correctly classified: ", testData[i].encode('ascii', 'ignore'), predictionLabels[i], testLabels[i])
